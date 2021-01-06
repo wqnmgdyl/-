94949be0394e82521f86591bcd0162c3cb31e411
wqnmgdyl <676411234@qq.com>
2021-01-06 16:21:50 +0800

    第二层级索引操作的实现

--- kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/handler/AdLevelDataHandler.java
+++ kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/handler/AdLevelDataHandler.java
@@ -1,6 +1,13 @@
 package com.kh.ad.handler;
 
+import com.kh.ad.dump.table.AdCreativeTable;
+import com.kh.ad.dump.table.AdPlanTable;
+import com.kh.ad.index.DataTable;
 import com.kh.ad.index.IndexAware;
+import com.kh.ad.index.adplan.AdPlanIndex;
+import com.kh.ad.index.adplan.AdPlanObject;
+import com.kh.ad.index.creative.CreativeIndex;
+import com.kh.ad.index.creative.CreativeObject;
 import com.kh.ad.mysql.constant.OpType;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,6 +18,34 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class AdLevelDataHandler {
+
+    public static void handleLevel2(AdPlanTable planTable, OpType type) {
+        AdPlanObject planObject = new AdPlanObject(planTable.getId(),
+                planTable.getUserId(),
+                planTable.getPlanStatus(),
+                planTable.getStartDate(),
+                planTable.getEndDate());
+        handleBinLogEvent(DataTable.of(AdPlanIndex.class),
+                planObject.getPlanId(),
+                planObject,
+                type);
+    }
+
+    public static void handleLevel2(AdCreativeTable creativeTable,OpType type) {
+        CreativeObject creativeObject = new CreativeObject(creativeTable.getAdId(),
+                creativeTable.getName(),
+                creativeTable.getType(),
+                creativeTable.getMaterialType(),
+                creativeTable.getHeight(),
+                creativeTable.getWidth(),
+                creativeTable.getAuditStatus(),
+                creativeTable.getAdUrl());
+        handleBinLogEvent(DataTable.of(CreativeIndex.class),
+                creativeObject.getAdId(),
+                creativeObject,
+                type);
+    }
+
     private static <K,V> void handleBinLogEvent(IndexAware<K,V> index,
                                                 K key,
                                                 V value,
