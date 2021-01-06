cf9927e2651c09537ed45ce1fa1b14cd86cd520c
wqnmgdyl <676411234@qq.com>
2021-01-06 11:01:28 +0800

    兴趣索引对象定义与服务实现

---
+++ kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/index/interest/UnitItObject.java
@@ -0,0 +1,16 @@
+package com.kh.ad.index.interest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author han.ke
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnitItObject {
+    private Long unitId;
+    private String itTag;
+}
