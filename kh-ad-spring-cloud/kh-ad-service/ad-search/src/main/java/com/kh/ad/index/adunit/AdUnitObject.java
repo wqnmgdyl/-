0ea060248b97513cf0898ce8dd929890c05e072c
wqnmgdyl <676411234@qq.com>
2021-01-06 10:14:59 +0800

    推广单元索引对象定义与服务实现

---
+++ kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/index/adunit/AdUnitObject.java
@@ -0,0 +1,40 @@
+package com.kh.ad.index.adunit;
+
+import com.kh.ad.index.adplan.AdPlanObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author han.ke
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdUnitObject {
+
+    private Long unitId;
+    private Integer unitStatus;
+    private Integer positionType;
+    private Long planId;
+
+    private AdPlanObject adPlanObject;
+
+    void update(AdUnitObject newObject) {
+        if (null != newObject.getUnitId()) {
+            this.unitId = newObject.getUnitId();
+        }
+        if (null != newObject.getUnitStatus()) {
+            this.unitStatus = newObject.getUnitStatus();
+        }
+        if (null != newObject.getPositionType()) {
+            this.positionType = newObject.getPositionType();
+        }
+        if (null != newObject.getPlanId()) {
+            this.planId = newObject.getPlanId();
+        }
+        if (null != newObject.getAdPlanObject()) {
+            this.adPlanObject = newObject.getAdPlanObject();
+        }
+    }
+}
