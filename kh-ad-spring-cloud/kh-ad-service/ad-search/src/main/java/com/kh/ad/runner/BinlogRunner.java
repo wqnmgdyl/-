3a1da10ba463e3602c330fa70b1f84ec328ff101
wqnmgdyl <676411234@qq.com>
2021-01-07 16:25:53 +0800

    启动对Binlog的监听

---
+++ kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/runner/BinlogRunner.java
@@ -0,0 +1,26 @@
+package com.kh.ad.runner;
+
+import com.kh.ad.mysql.BinlogClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author han.ke
+ */
+@Slf4j
+@Component
+public class BinlogRunner implements CommandLineRunner {
+
+    private final BinlogClient client;
+
+    public BinlogRunner(BinlogClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Coming in BinlogRunner...");
+        client.connect();
+    }
+}
