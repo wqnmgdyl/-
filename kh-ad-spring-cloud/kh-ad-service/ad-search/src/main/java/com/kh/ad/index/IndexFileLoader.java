10ebc566bf7c10d9e359efb2237db76448a20655
wqnmgdyl <676411234@qq.com>
2021-01-07 09:45:05 +0800

    全量索引加载的实现

---
+++ kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/index/IndexFileLoader.java
@@ -0,0 +1,79 @@
+package com.kh.ad.index;
+
+import com.alibaba.fastjson.JSON;
+import com.kh.ad.dump.DConstant;
+import com.kh.ad.dump.table.*;
+import com.kh.ad.handler.AdLevelDataHandler;
+import com.kh.ad.mysql.constant.OpType;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 根据数据表导出的文件，读取文件，加载索引
+ * @author han.ke
+ */
+@Component
+@DependsOn("dataTable")
+public class IndexFileLoader {
+
+    @PostConstruct
+    public void init() {
+        List<String> adPlanStrings = loadDumpData(String.format("%s%s",
+                DConstant.DATA_ROOT_DIR,DConstant.AD_PLAN));
+        adPlanStrings.forEach(p -> AdLevelDataHandler.handleLevel2(
+                JSON.parseObject(p, AdPlanTable.class),OpType.ADD
+        ));
+
+        List<String> adCreativeStrings = loadDumpData(String.format("%s%s",
+                DConstant.DATA_ROOT_DIR,DConstant.AD_CREATIVE));
+        adCreativeStrings.forEach(c -> AdLevelDataHandler.handleLevel2(
+                JSON.parseObject(c, AdCreativeTable.class),OpType.ADD
+        ));
+
+        List<String> adUnitStrings = loadDumpData(String.format("%s%s",
+                DConstant.DATA_ROOT_DIR,DConstant.AD_UNIT));
+        adUnitStrings.forEach(u -> AdLevelDataHandler.handleLevel3(
+                JSON.parseObject(u, AdUnitTable.class),OpType.ADD
+        ));
+
+        List<String> adCreativeUnitStrings = loadDumpData(String.format("%s%s",
+                DConstant.DATA_ROOT_DIR,DConstant.AD_CREATIVE_UNIT));
+        adCreativeUnitStrings.forEach(cu -> AdLevelDataHandler.handleLevel3(
+                JSON.parseObject(cu, AdCreativeUnitTable.class),OpType.ADD
+        ));
+
+        List<String> adUnitDistrictStrings = loadDumpData(String.format("%s%s",
+                DConstant.DATA_ROOT_DIR,DConstant.AD_UNIT_DISTRICT));
+        adUnitDistrictStrings.forEach(d -> AdLevelDataHandler.handleLevel4(
+                JSON.parseObject(d, AdUnitDistrictTable.class),OpType.ADD
+        ));
+
+        List<String> adUnitItStrings = loadDumpData(String.format("%s%s",
+                DConstant.DATA_ROOT_DIR,DConstant.AD_UNIT_IT));
+        adUnitItStrings.forEach(i -> AdLevelDataHandler.handleLevel4(
+                JSON.parseObject(i, AdUnitItTable.class),OpType.ADD
+        ));
+
+        List<String> adUnitKeywordStrings = loadDumpData(String.format("%s%s",
+                DConstant.DATA_ROOT_DIR,DConstant.AD_UNIT_KEYWORD));
+        adUnitKeywordStrings.forEach(k -> AdLevelDataHandler.handleLevel4(
+                JSON.parseObject(k, AdUnitKeywordTable.class),OpType.ADD
+        ));
+    }
+
+    private List<String> loadDumpData(String fileName) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))) {
+            return br.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
