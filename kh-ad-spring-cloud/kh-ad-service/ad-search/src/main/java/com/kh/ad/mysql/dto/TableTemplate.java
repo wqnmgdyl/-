540b2f9173067baf13d90fe8cef7af37ce680e17
wqnmgdyl <676411234@qq.com>
2021-01-07 10:58:05 +0800

    构造解析Binlog使用到的模板文件

---
+++ kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/mysql/dto/TableTemplate.java
@@ -0,0 +1,27 @@
+package com.kh.ad.mysql.dto;
+
+import com.kh.ad.mysql.constant.OpType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author han.ke
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TableTemplate {
+    private String tableName;
+    private String level;
+
+    private Map<OpType, List<String>> opTypeFieldSetMap = new HashMap<>();
+    /**
+     * 字段索引 -> 字段名
+     */
+    private Map<Integer, String> posMap = new HashMap<>();
+}
---
+++ kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/mysql/dto/Template.java
@@ -0,0 +1,18 @@
+package com.kh.ad.mysql.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author han.ke
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Template {
+    private String database;
+    private List<JsonTable> tableList;
+}
