8b39ea2ecb953cd8ebb9dc700a1ab2df3c6cac84
wqnmgdyl <676411234@qq.com>
2021-01-08 09:29:59 +0800

    索引层级枚举类

--- kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/index/DataLevel.java
+++ kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/index/DataLevel.java
@@ -1,19 +1,20 @@
 package com.kh.ad.index;
 
 /**
+ * 索引层级
+ *
  * @author han.ke
  */
-
 public enum DataLevel {
 
-    LEVEL2("2","level 2"),
-    LEVEL3("3","level 3"),
-    LEVEL4("4","level 4");
+    LEVEL2("2", "level 2"),
+    LEVEL3("3", "level 3"),
+    LEVEL4("4", "level 4");
 
     private String level;
     private String desc;
 
-    DataLevel(String level,String desc) {
+    DataLevel(String level, String desc) {
         this.level = level;
         this.desc = desc;
     }
