f7d25a23effc29c6683f4d5ad130fd109d967aff
wqnmgdyl <676411234@qq.com>
2021-01-11 11:17:33 +0800

    完善广告检索服务

--- kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/controller/SearchController.java
+++ kh-ad-spring-cloud/kh-ad-service/ad-search/src/main/java/com/kh/ad/controller/SearchController.java
@@ -5,6 +5,9 @@ import com.kh.ad.annotation.IgnoreResponseAdvice;
 import com.kh.ad.client.SponsorClient;
 import com.kh.ad.client.vo.AdPlan;
 import com.kh.ad.client.vo.AdPlanGetRequest;
+import com.kh.ad.search.ISearch;
+import com.kh.ad.search.vo.SearchRequest;
+import com.kh.ad.search.vo.SearchResponse;
 import com.kh.ad.vo.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +25,20 @@ import java.util.List;
 @RestController
 public class SearchController {
 
+    private final ISearch search;
     private final RestTemplate restTemplate;
     private final SponsorClient sponsorClient;
 
-    public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient) {
+    public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient,ISearch search) {
         this.restTemplate = restTemplate;
         this.sponsorClient = sponsorClient;
+        this.search = search;
+    }
+
+    @PostMapping("/fetchAds")
+    public SearchResponse fetchAds(@RequestBody SearchRequest request){
+        log.info("ad-search: fetchAds -> {}",JSON.toJSONString(request));
+        return search.fetchAds(request);
     }
 
     @IgnoreResponseAdvice
