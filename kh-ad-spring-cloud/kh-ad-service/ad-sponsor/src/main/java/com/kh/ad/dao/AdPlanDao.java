4277b64e78bb458b858098a986ce8516cce51526
wqnmgdyl <676411234@qq.com>
2021-01-13 15:50:13 +0800

    --

--- kh-ad-spring-cloud/kh-ad-service/ad-sponsor/src/main/java/com/kh/ad/dao/AdPlanDao.java
+++ kh-ad-spring-cloud/kh-ad-service/ad-sponsor/src/main/java/com/kh/ad/dao/AdPlanDao.java
@@ -4,8 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kh.ad.entity.AdPlan;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 /**
  * @author han.ke
  */
